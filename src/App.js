8b3fba2fa980f4d0002fcadb57a340787975115e
Thebks <ichbks@gmail.com>
2023-02-27 15:21:21 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
+import './normal.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <aside className="sidemenu">
+        <div className="sidemenu-button">
+          <svg class="h-4 w-4" width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" >
+            <line x1="0" y1="6" x2="12" y2="6" stroke="currentColor" fill="none" stroke-width="2" />
+            <line x1="6" y1="0" x2="6" y2="12" stroke="currentColor" fill="none" stroke-width="2" />
+          </svg>
+
+          New Chat
+        </div>
+      </aside >
+      <section class="chat-section">
+
+      </section>
+    </div >
   );
 }
 
